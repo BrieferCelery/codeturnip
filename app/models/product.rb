e34925ec9f77bdcac7f85ab0d673d5ffedc559a1
Aaron Kane <aaronkane@192-168-1-128.tpgi.com.au>
2020-05-14 15:22:26 +1000

    validates added to prod model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
-    has_many :user_product_lists
+    has_many :users, through: :user_product_lists
     has_one_attached :picture
+
+    validates :name, presence: true
 end
