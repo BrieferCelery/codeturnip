c9fba3e196603e64ccc5c016f8cb385e5b0c8e03
Aaron Kane <aaronkane@192-168-1-128.tpgi.com.au>
2020-05-19 01:55:07 +1000

    finished stripe

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,7 @@ class Product < ApplicationRecord
     has_one_attached :picture
     validates :name, presence: true
     validates :price, presence: true
+    validates :picture, presence: true
 
     has_many :categories, through: :products_category
     
