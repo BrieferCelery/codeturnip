5760e012e672ec1b7bae8d189a8cf8f45538d8a3
Aaron Kane <aaronkane@192-168-1-128.tpgi.com.au>
2020-05-18 15:54:40 +1000

    glen's commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_17_130001) do
+ActiveRecord::Schema.define(version: 2020_05_16_150344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,9 +43,9 @@ ActiveRecord::Schema.define(version: 2020_05_17_130001) do
   end
 
   create_table "products", force: :cascade do |t|
-    t.string "name"
-    t.decimal "price"
-    t.integer "supply"
+    t.string "name", null: false
+    t.decimal "price", null: false
+    t.integer "supply", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -67,15 +67,6 @@ ActiveRecord::Schema.define(version: 2020_05_17_130001) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
     t.index ["resource_type", "resource_id"], name: "index_roles_on_resource_type_and_resource_id"
-  end
-
-  create_table "user_product_lists", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "product_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["product_id"], name: "index_user_product_lists_on_product_id"
-    t.index ["user_id"], name: "index_user_product_lists_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -101,6 +92,4 @@ ActiveRecord::Schema.define(version: 2020_05_17_130001) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "products_categories", "categories"
   add_foreign_key "products_categories", "products"
-  add_foreign_key "user_product_lists", "products"
-  add_foreign_key "user_product_lists", "users"
 end
