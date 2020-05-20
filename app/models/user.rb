c5233fead79775a57658a6aa590c4e2d000aea8f
Aaron Kane <aaronkane@192-168-1-128.tpgi.com.au>
2020-05-21 03:48:40 +1000

    finished?

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
-  has_many :products
-
   has_many :posts
 end
